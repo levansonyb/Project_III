2377629c35ab56dfce5f31d77cb1ca6b3dc21815
levansonyb <levansonyb2003@gmail.com>
2024-12-25 16:31:58 +0700

    fix OnTheDrag

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public abstract class Board : MonoBehaviour
+public class Board : MonoBehaviour
 {
     public GameObject CellObject;
 
